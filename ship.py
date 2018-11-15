4efd550c1b95b251450d4d31376e69a1712ab576
Matthew Bowe <bowematthew13@gmail.com>
2018-11-15 16:59:43 -0500

    refactored game functions and stopped ship from going off the screen

--- ship.py
+++ ship.py
@@ -3,10 +3,10 @@ from PIL import Image
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize ship and set starting position."""
         self.screen = screen
-
+        self.ai_settings = ai_settings
         # Load ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -15,14 +15,21 @@ class Ship():
         #Start each new ship at the bottom center of screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-
-        # Movement flag
+        # Store a decimal value at the bottom center of screen.
+        self.center = float(self.rect.centerx)
+        # Movement flags
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on movement flag."""
-        if self.moving_right:
-            self.rect.centerx += 1
+        """Update the ship's position based on movement flags."""
+        # Update the ships center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        # Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship and its current location."""
