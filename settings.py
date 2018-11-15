b7aadc2e34ccf340a695c0317460287164fdaeb1
Matthew Bowe <bowematthew13@gmail.com>
2018-11-15 17:35:16 -0500

    added bullet class and functionality

--- settings.py
+++ settings.py
@@ -1,8 +1,13 @@
 class Settings():
     # Setting for the screen in the alien game.
     def __init__(self):
-        self.screen_width=1200
-        self.screen_height=800
-        self.bg_color=(230,230,230)
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
         # Ship setting
         self.ship_speed_factor = 1.5
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
